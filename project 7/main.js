28e627ffa9573b734c71ac7c410c2696db706198
Abdellah <abdellahkarani@gmail.com>
2024-03-26 22:22:49 +0000

    Add DOM Traversing But Not Complete

--- project 7/main.js
+++ project 7/main.js
@@ -1,19 +1,27 @@
-var btn_s = document.querySelectorAll(".showPlus");
-var question = document.querySelector(".question");
-btn_s.forEach(element => {
-    element.addEventListener("click", function(event) {
-        if (element.children[0].classList.contains("fa-minus")) {
-            element.children[0].className = 'fa-sharp fa-solid fa-plus';
-        } else {
-            element.children[0].className = 'fa-sharp fa-solid fa-minus';
-        }
-        element.parentElement.parentElement.classList.toggle("show-text");
+// var btn_s = document.querySelectorAll(".showPlus");
+// var question = document.querySelector(".question");
+// var icon = document.querySelector(".icon");
+// btn_s.forEach(element => {
+//     element.addEventListener("click", function(event) {
+//         element.parentElement.parentElement.classList.toggle("show-text");
+//         event.currentTarget.firstElementChild.classList.toggle("minus");
+//     });
+// });
+
+// Traversing The DOM
+var qs = document.querySelectorAll(".question");
+qs.forEach(function(question) {
+    const btn = question.querySelector(".showPlus");
+    btn.addEventListener("click" , function() {
+        btn.firstElementChild.classList.toggle("minus");
+        qs.forEach(function(item) {
+            if (item !== question) {
+                item.classList.remove("show-text");
+            }
+        });
+        question.classList.toggle("show-text");
     });
 });
-
-
-
-
 
 
 
