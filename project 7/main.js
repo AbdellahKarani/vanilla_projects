0afef89a9d7eacbb6010bb3fff3c02ca879bc648
Abdellah <abdellahkarani@gmail.com>
2024-03-27 22:08:56 +0000

    Finich Project 7

--- project 7/main.js
+++ project 7/main.js
@@ -1,28 +1,19 @@
-// var btn_s = document.querySelectorAll(".showPlus");
-// var question = document.querySelector(".question");
-// var icon = document.querySelector(".icon");
-// btn_s.forEach(element => {
-//     element.addEventListener("click", function(event) {
-//         element.parentElement.parentElement.classList.toggle("show-text");
-//         event.currentTarget.firstElementChild.classList.toggle("minus");
-//     });
-// });
-
 // Traversing The DOM
-var qs = document.querySelectorAll(".question");
-qs.forEach(function(question) {
+var questions = document.querySelectorAll(".question");
+questions.forEach(function(question) {
     const btn = question.querySelector(".showPlus");
-    btn.addEventListener("click" , function() {
-        btn.firstElementChild.classList.toggle("minus");
-        qs.forEach(function(item) {
+    btn.addEventListener("click" , function(e) {
+        questions.forEach(function(item) {
             if (item !== question) {
                 item.classList.remove("show-text");
+                // item.firstElementChild.lastElementChild.firstElementChild.classList.remove("minus");
+                item.querySelector(".icon").classList.remove("minus");
             }
         });
+        btn.firstElementChild.classList.toggle("minus");
         question.classList.toggle("show-text");
     });
 });
-
 
 
 
