f03690864448b2f56db668e32f4bceacb278c046
Abdellah <abdellahkarani@gmail.com>
2024-04-09 21:37:02 +0000

    .

--- project 10/main.js
+++ project 10/main.js
@@ -1,7 +1,33 @@
+var header = document.querySelector("header");
+var logo = document.querySelector(".logo");
+var links = document.querySelectorAll("header ul li a");
+window.onscroll = function() {
+    if (window.scrollY > 200) {
+        header.classList.add("scroll");
+        logo.classList.add("scroll");
+        links.forEach(function(ele) {
+            ele.classList.add("scroll");
+        });
+    }
+    else {
+        header.classList.remove("scroll");
+        logo.classList.remove("scroll");
+        links.forEach(function(ele) {
+            ele.classList.remove("scroll");
+        });
+    }
+}
+
+var explore = document.querySelector("#explore");
+explore.addEventListener("click", function(event) {
+    event.preventDefault();
+    scrollBy(0, window.screen.height * 3);
+});
 
 
-
-
+document.addEventListener("DOMContentLoaded", function() {
+    scrollTo(0, -window.screen.height * 3);
+});
 
 
 
