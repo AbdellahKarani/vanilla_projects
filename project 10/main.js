7765468951af73abf6ca77b8068742f3cf056f9e
Abdellah <abdellahkarani@gmail.com>
2024-04-13 23:56:22 +0000

    .

--- project 10/main.js
+++ project 10/main.js
@@ -1,45 +1,71 @@
+// ************* Set The Date *************
+var date = document.querySelector(".data");
+date.innerHTML = new Date().getFullYear();
+
+// ************* Fixed NavBar && Fix Up Scroll *************
 var header = document.querySelector("header");
 var logo = document.querySelector(".logo");
-var links = document.querySelectorAll("header ul li a");
+var ul_links = document.querySelector("header ul.links");
+var up = document.querySelector(".up");
 window.onscroll = function() {
     if (window.scrollY > 200) {
-        header.classList.add("scroll");
-        logo.classList.add("scroll");
-        links.forEach(function(ele) {
-            ele.classList.add("scroll");
-        });
+        header.classList.add("setHeaderColor");
+        logo.classList.add("setColorLogo");
+        ul_links.classList.add("set-color");
     }
     else {
-        header.classList.remove("scroll");
-        logo.classList.remove("scroll");
-        links.forEach(function(ele) {
-            ele.classList.remove("scroll");
-        });
+        header.classList.remove("setHeaderColor");
+        ul_links.classList.remove("set-color");
+        logo.classList.remove("setColorLogo");
+    }
+    // Start A Up Scroll
+    if (window.scrollY > 400) {
+        up.classList.add("show");
+    }
+    else {
+        up.classList.remove("show");
     }
 }
-
-var explore = document.querySelector("#explore");
-explore.addEventListener("click", function(event) {
-    event.preventDefault();
-    scrollBy(0, window.screen.height * 3);
+up.addEventListener("click", function() {
+    scrollTo(0, -window.screen.height);
 });
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    scrollTo(0, -window.screen.height * 3);
-});
-
-// Start Bars Section In Header 
+// ************* Set Nav Bar *************
 var bar = document.querySelector("header .bars");
 var ul_links = document.querySelector("header ul.links");
+var links = document.querySelector("header .links-container");
 bar.addEventListener("click", function() {
-    ul_links.classList.toggle("down");
-    if (ul_links.classList.contains("down")) {
+    const container_height = ul_links.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    console.log(container_height);
+    console.log(linksHeight);
+    if (linksHeight == 0) {
+        links.style.height = `${container_height}px`;
+        links.visibility = "visible";
+        links.padding = "0 10px";
+    } else {
+        links.style.height = 0;
+    }
+    if (linksHeight == 0) {
         bar.className = "fa-solid fa-xmark bars";
         bar.style.color = "red";
     } else {
         bar.className = "fa-solid fa-bars bars";
         bar.style.color ="#102a42";
     }
+    // links.classList.toggle("show_links");
+});
+
+// ************* Explore Scroll *************
+var explore = document.querySelector("#explore");
+explore.addEventListener("click", function(event) {
+    event.preventDefault();
+    scrollBy(0, window.screen.height * 3);
+});
+
+// ************* Loading The Page *************
+document.addEventListener("DOMContentLoaded", function() {
+    scrollTo(0, -window.screen.height * 3);
 });
 
---
+++ refernce/main.js
@@ -0,0 +1,9 @@
+// get The Height Auto And Modified In Auto Way
+
+var ul_links = document.querySelector("header ul.links");
+var links = document.querySelector("header .links-container");
+const container_height=ul_links.getBoundingClientRect().height;
+const linksHeight=links.getBoundingClientRect().height; 
+
+
+
