85f80003bbfb4a4d0ce92208af98340d964be94a
Abdellah <abdellahkarani@gmail.com>
2024-04-19 16:19:09 +0100

    .

--- refernce/main.js
+++ refernce/main.js
@@ -15,9 +15,26 @@ const linksHeight=links.getBoundingClientRect().height;
     Use getBoundingClientRect().height for total height, 
     offsetTop for distance from the top edge.
 */
-var element = document.querySelector(".menu");
+var element = document.querySelector(".links");
 const position1 = element.getBoundingClientRect().height;
 const position2 = element.offsetTop;
+
+
+// Target and Dataset
+/*
+    <div class="dataset" data-id="active">Data Set</div>
+*/
+var btn = document.querySelector(".click-here");
+btn.addEventListener("click", function(e) {
+    console.log(e.currentTarget.dataset.id); // active
+    console.log(e.target.dataset.id); // active
+});
+
+
+
+
+
+
 
 
 
